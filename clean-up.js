8e59ab9de51175d57be20dece8cd81f4435997bc
Didier Catz <didierlennon@gmail.com>
2020-10-21 19:10:50 +0200

    Sanitize data

---
+++ clean-up.js
@@ -0,0 +1,94 @@
+const csv = require('csvtojson')
+
+const animals = [
+  { 
+    includes: 'kat',
+    name: 'Kat'
+  },
+  { 
+    includes: 'rat',
+    name: 'Rat'
+  },
+  { 
+    includes: 'hon', // One person wrote 'hond' with a 't'... (hont) 🤦‍♂️
+    name: 'Hond'
+  },
+  { 
+    includes: 'dwergteckel',
+    name: 'Hond'
+  },
+  { 
+    includes: 'vogel',
+    name: 'Vogel'
+  },
+  { 
+    includes: 'konijn',
+    name: 'Konijn'
+  },
+  { 
+    includes: 'hamster',
+    name: 'Hamster'
+  },
+  { 
+    includes: 'kip',
+    name: 'Kip'
+  },
+]
+
+async function cleanUp() {
+
+  // Path to CSV file
+  const csvFilePath = './data/data.csv'
+
+  // Convert CSV to JSON
+  const convertToJSON = async (filePath) => csv().fromFile(filePath)
+
+  // Get JSON data
+  const json = await convertToJSON(csvFilePath)
+
+  // Get only the animals row
+  const petsData = json.map((entry) => {
+    return entry['Welke huisdieren heb je? (naam; soort dier: scheiden door punt komma\'s, \'geen\' voor geen huisdier)']
+  })
+  
+  // Get cleaned array of pets
+  const cleanPets = petsData.map(entry => {
+    // Replace blank entries with empty string
+    const geen = new RegExp('geen', 'g')
+    const space = new RegExp(' ', 'g')
+    const nvt = new RegExp('n>v>t>', 'g')
+    
+    return entry
+      .toLowerCase()
+      .replace(geen, '')
+      .replace(/'/g, '')
+      .replace(/-/g, '')
+      .replace(nvt, '')
+  })
+    
+    // Correct all animal names
+    .map((entry) => entry
+      .replace('hont', 'hond')
+      .replace('dwerg teckel', 'hond'))
+
+    // Remove all spaces and numbers
+    .map((entry) => {
+      return entry
+        .replace(/[0-9]/g, '')
+        .replace(/ /g, ';')
+    })
+    // replace all strings that are not animals
+    .map(entry => entry)
+    
+    .sort()
+    // Replace all empty values with null
+    .map(entry =>
+      entry === ''
+        ? null
+        : entry
+    )
+
+  console.log(cleanPets)
+}
+
+cleanUp()
--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const { API_KEY_NAME, API_KEY_ID, API_SECRET, APP_TOKEN, APP_SECRET } = process.
 const TESLA_API = 'https://opendata.rdw.nl/resource/y4vt-i24s.json'
 
 const api = fetch(`${TESLA_API}?$$app_token=${APP_TOKEN}`)
-            .then(response => response.json())
-            .then(data => console.log(data));
+  .then(response => response.json())
+  .then(data => console.log(data))
